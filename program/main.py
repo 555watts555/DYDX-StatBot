e5a9f6b41a968c181c002868b02ad51b290f8482
Mitchell Watts <mitchellwatts@mitchells-mbp.mynetworksettings.com>
2024-01-18 10:57:59 -0500

    Stage 2 Start

--- program/main.py
+++ program/main.py
@@ -1,4 +1,37 @@
+from func_connections import connect_dydx
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_private import abort_all_positions
+from func_public import construct_market_prices
 
-
+#CONNECT TO CLIENT
 if __name__ == "__main__":
-    print("hello")
+    try:
+        print("CONNECTING TO CLIENT...")
+        client = connect_dydx()
+    except Exception as e:
+        print(e)
+        print("error connecting to client: ", e)
+        exit(1)
+
+#ABORT ALL OPEN POSITIONS
+if ABORT_ALL_POSITIONS:
+    try:
+      print("Closing all positions...")
+      close_orders = abort_all_positions(client)
+    except Exception as e:
+      print("Error closing all positions: ", e)
+      exit(1)
+
+    # Find Cointegrated Pairs
+if FIND_COINTEGRATED:
+
+    # Construct Market Prices
+    try:
+      print("Fetching market prices, please allow 3 mins...")
+      df_market_prices = construct_market_prices(client)
+    except Exception as e:
+      print("Error constructing market prices: ", e)
+      send_message(f"Error constructing market prices {e}")
+      exit(1)
+
+   
